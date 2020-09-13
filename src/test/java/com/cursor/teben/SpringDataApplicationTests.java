7d9c6abc694fbbef86728de14d55242f3dda9399
Artur Teben <teben.artur99@gmail.com>
2020-09-13 21:02:27 +0300

    Add tests

---
+++ src/test/java/com/cursor/teben/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursor.teben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
